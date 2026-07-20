7d83275a45b9d7f8654c7007bf381ea4063777a1
Aman <amanrajverma003@gmail.com>
2026-07-21 00:02:48 +0530

    feat: bootstrap spring boot application

---
+++ src/main/java/com/transakt/transakt/TransaktApplication.java
@@ -0,0 +1,13 @@
+package com.transakt.transakt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransaktApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransaktApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/transakt/transakt/TransaktApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transakt.transakt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransaktApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
