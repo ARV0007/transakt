2f871cc2c788b3be577d9a73e04657d840faca3e
Aman <amanrajverma003@gmail.com>
2026-09-07 20:36:46 +0530

    config: exclude the default UserDetailsService, unused by both auth doors

--- src/main/java/com/transakt/transakt/TransaktApplication.java
+++ src/main/java/com/transakt/transakt/TransaktApplication.java
@@ -3,9 +3,10 @@ package com.transakt.transakt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
 @EnableScheduling
-@SpringBootApplication
+@SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 public class TransaktApplication {
 
 	public static void main(String[] args) {
